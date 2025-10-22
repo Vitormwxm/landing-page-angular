d1f101afd80a7dd1b79776a33b8b2ef0179d6ac1
vitor <vitormwxm@gmail.com>
2025-10-21 23:57:31 -0300

    create button component

--- landing-page/src/app/components/button/button.component.ts
+++ landing-page/src/app/components/button/button.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
+  standalone: true,
   imports: [],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
+  @Input("btnText") btnText: String = "";
 
+
+  submit() {
+
+  }
 }
