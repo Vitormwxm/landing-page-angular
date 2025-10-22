8072faa26c4d5a397dda85bc714181d37fc4c10b
vitor <vitormwxm@gmail.com>
2025-10-22 00:39:17 -0300

    create the principal section

--- landing-page/src/app/components/home/home.component.ts
+++ landing-page/src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, ButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
